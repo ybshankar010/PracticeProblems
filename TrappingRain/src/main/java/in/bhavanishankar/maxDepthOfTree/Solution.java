95845cb4b17653a6fb501c458f15f6a049f015ad
Shankar Y Bhavani <yeleswarapushankar@gmail.com>
2026-09-03 10:56:06 +0530

    Max Depth of the tree

---
+++ TrappingRain/src/main/java/in/bhavanishankar/maxDepthOfTree/Solution.java
@@ -0,0 +1,30 @@
+package in.bhavanishankar.maxDepthOfTree;
+
+import com.sun.source.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth) + 1;
+    }
+}
